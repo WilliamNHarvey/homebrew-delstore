ae403cec72abc15c46c0e870453fb5a4809f3f14
WilliamNHarvey <williamnharvey@gmail.com>
2019-04-07 15:04:49 -0700

    install from github

--- delstore.rb
+++ delstore.rb
@@ -1,9 +1,9 @@
 class Delstore < Formula
   desc "Delete .DS_Store recursively in a folder"
-  homepage "https://williamnharvey.com"
-  url "https://williamnharvey.com/assets/files/delstore.tar"
+  homepage "https://github.com/WilliamNHarvey/delstore"
+  url "https://raw.githubusercontent.com/WilliamNHarvey/delstore/master/delstore"
   version "0.1.0"
-  sha256 ""
+  sha256 "c427f7655458bd0066614e6090488481347d11eb4c4101b3c2b8efb0b16463f8"
 
   def install
     bin.install "delstore"
