9ae7edaddbade96abdb62c364a89ba439f536377
WilliamNHarvey <williamnharvey@gmail.com>
2019-04-07 15:55:19 -0700

    change repo name

--- delstore.rb
+++ delstore.rb
@@ -1,7 +1,7 @@
 class Delstore < Formula
   desc "Delete .DS_Store recursively in a folder"
-  homepage "https://github.com/WilliamNHarvey/delstore"
-  url "https://raw.githubusercontent.com/WilliamNHarvey/delstore/master/delstore"
+  homepage "https://github.com/WilliamNHarvey/homebrew-delstore"
+  url "https://raw.githubusercontent.com/WilliamNHarvey/homebrew-delstore/master/delstore"
   version "0.1.0"
   sha256 "f393f2e69ccd55bcd3c368440c7a8bc725f347d5c5954f9791e53983cb016267"
 
