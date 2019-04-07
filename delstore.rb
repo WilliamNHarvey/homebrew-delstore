ed6573b231527004b255d18f5f741c3c9cb0917c
WilliamNHarvey <williamnharvey@gmail.com>
2019-04-07 15:46:28 -0700

    update sha

--- delstore.rb
+++ delstore.rb
@@ -3,7 +3,7 @@ class Delstore < Formula
   homepage "https://github.com/WilliamNHarvey/delstore"
   url "https://raw.githubusercontent.com/WilliamNHarvey/delstore/master/delstore"
   version "0.1.0"
-  sha256 "c427f7655458bd0066614e6090488481347d11eb4c4101b3c2b8efb0b16463f8"
+  sha256 "f393f2e69ccd55bcd3c368440c7a8bc725f347d5c5954f9791e53983cb016267"
 
   def install
     bin.install "delstore"
